e562f921758ecfa808fa8818ca96f789e682bf71
AngelTroncoso <157044866+AngelTroncoso@users.noreply.github.com>
2024-07-09 19:09:12 -0400

    ricomida original

---
+++ Asset/javascript/archivo.js
@@ -0,0 +1,34 @@
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip1"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+const tooltipTriggerList1 = document.querySelectorAll('[data-bs-toggle="tooltip2"]')
+const tooltipList2 = [...tooltipTriggerList1].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+ 
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
