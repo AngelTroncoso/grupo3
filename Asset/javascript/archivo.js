e6380727f1a962e9a249a57918896d78b66b81bc
lioht <lioht@gmail.com>
2024-07-09 19:56:56 -0400

    tarjetas y js

--- Asset/javascript/archivo.js
+++ Asset/javascript/archivo.js
@@ -5,7 +5,23 @@ const tooltipTriggerList1 = document.querySelectorAll('[data-bs-toggle="tooltip2
 const tooltipList2 = [...tooltipTriggerList1].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
  
  
- 
+$(document).ready(function() {
+    $('.card-title1').click(function() {
+        $('.card-text1').toggle();
+    });
+});
+
+$(document).ready(function() {
+    $('.card-title2').click(function() {
+        $('.card-text2').toggle();
+    });
+});
+
+$(document).ready(function() {
+    $('.card-title3').click(function() {
+        $('.card-text3').toggle();
+    });
+});
 
 
 
